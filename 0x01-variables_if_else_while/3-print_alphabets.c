ecadac0b8b20518c23fb680faa144277a8a5a603
gab-rinho1 <gri75407@gmail.com>
2023-08-11 12:52:36 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@
  *
  */
 
-int main (void)
+int main(void)
 
 {
 	char low;
@@ -20,7 +20,7 @@ int main (void)
 	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
 	putchar('\n');
 
