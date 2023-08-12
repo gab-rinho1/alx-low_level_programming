00501aaab9c32b9d020ca3eefe10156ff1761629
gab-rinho1 <gri75407@gmail.com>
2023-08-11 19:11:01 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: Always 0 (Greatness)
  */
 
 int main(void)
 
 {
-	int d;
+	int hd;
 	char low;
 
-	for (d = '0'; d <= '9'; low++)
+	for (hd = '0'; hd <= '9'; hd++)
+	putchar(hd);
+
+	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
 
