b709dd3d65676411eb5e93e368dbb8995b084fc3
gab-rinho1 <gri75407@gmail.com>
2023-08-22 16:25:58 -0700

    ramdom password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,37 +3,53 @@
 #include <time.h>
 
 /**
- * main - greatst random void password
+ * main - greatest random void password
  * for the 101-crackme
  * Return: Always 0
  */
 
 int main(void)
-
 {
-	char password[04];
+	char password[84];
 	int index = 0; sum = 0; diff_half1; diff_half2;
 	
-	srand (time[0]);
+	srand (time(0));
 
 	while (sum < 2772)
+
 	{
-	password[index] = 33 + rand[] % 94;
-	sum += password[index++];
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
+
 	password[index] = '\0';
 
 	if (sum != 2772)
 	{
+
 	diff_half1 = (sum - 2772) / 2;
 	diff_half2 = (sum - 2772) / 2;
 
 	if ((sum - 2772) % 2 != 0)
 	diff_half1++;
 	
-	for (index = 0; password[index]; index++;
+	for (index = 0; password[index]; index++)
 	{
 	if (password[index] >= (33 + diff_half1))
 	{
-
-
+	password[index] -= diff_half1;
+	break;
+	}
+	}
+	for (index = 0; password[index]; index++)
+	
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half2;
+	break;
+	}
+	}
+	}
+	printf("%s", password);
+	return (0);
+}
