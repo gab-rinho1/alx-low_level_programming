0dd132450626ca5f9643df6f82f37c95a6fb91bb
gab-rinho1 <gri75407@gmail.com>
2023-08-23 14:36:32 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,53 +3,43 @@
 #include <time.h>
 
 /**
- * main - greatest random void password
+ * main - greatst random void password
  * for the 101-crackme
  * Return: Always 0
  */
 
 int main(void)
+
 {
-	char password[84];
-	int index = 0; sum = 0; diff_half1; diff_half2;
-	
-	srand (time(0));
+	int a, b;
+	int j, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	while (sum < 2772)
-
+	srand(time(0));
+	while (s != 2772)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
+	a = b = s = 0;
+	while ((2772 - 122) > s)
+	{
+	j = rand() % 62;
+	p[a] = c[j];
+	s += c[j];
+	a++;
 	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
+	while (c[b])
 	{
-
-	diff_half1 = (sum - 2772) / 2;
-	diff_half2 = (sum - 2772) / 2;
-
-	if ((sum - 2772) % 2 != 0)
-	diff_half1++;
-	
-	for (index = 0; password[index]; index++)
+	if (c[b] == (2772 - s))
 	{
-	if (password[index] >= (33 + diff_half1))
-	{
-	password[index] -= diff_half1;
+	p[a] = c[b];
+	s += c[b];
+	a++;
 	break;
 	}
-	}
-	for (index = 0; password[index]; index++)
-	
-	if (password[index] >= (33 + diff_half2))
-	{
-	password[index] -= diff_half2;
-	break;
+	b++;
 	}
 	}
-	}
-	printf("%s", password);
+	p[a] = '\0';
+	printf("%s\n", p);
 	return (0);
 }
