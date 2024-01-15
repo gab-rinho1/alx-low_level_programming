36d11f6e69ed977498ef77ead1cd10dbecd61a01
gab-rinho1 <gri75407@gmail.com>
2024-01-15 08:05:45 -0800

    a_stressful_week

---
+++ 0x0C-python-almost_a_circle/10-main.py
@@ -0,0 +1,2 @@
+VimCrypt~03!¶áˆšŠ* ã¤Òº×˜9‡çjÄÅëÀ<÷£éoP'tj9+NW	g„Ê0·æàQJ¥ç`¾¨E›ø»¶(,n=¨8gü»’ÃÉ”ò^KÌxm¹s¨y£¥:/Þ¦(&®Yj^Ý!«¹=eã©‚:?¡c-µ+‹\xf¦eúÓNûbÇ¯fd
+©°°„¯@Žø/àÉåŒ$†0|ßzHEC4kÙê|fi‚FèµpU©ïLp¢*|LŸ³dCœ:¥âFú·tŸ¡kƒN†cX«ê{?|oçCÔd¤Œ+’ÞÎô\;E÷]ÂT'°D¾^i6{Œ6æ_Â\9{ì7,ª»½—£ÍˆúbN½#ìhp€÷ÑP¿¤¡š'ŒmºýwhR
